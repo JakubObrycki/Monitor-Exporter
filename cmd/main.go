8d7e85890bd0537a5d294966267d7f32202fbccd
faftoo <faftoo@debian.fafto>
2025-04-28 12:57:23 -0400

    monitor_v1

--- cmd/main.go
+++ cmd/main.go
@@ -1,32 +1,5 @@
 package main
 
-import (
-	"cpu/internal/monitor"
-	"log"
-)
-
 func main() {
-	data, err := monitor.MonitorCpu() // pomyslec o go routines
-	if err != nil {
-		log.Printf("MonitorCpu error: %v", err)
-	}
-	log.Println("Total CPU:", data.CPU, "%")
-
-	dataMem, err := monitor.MonitorMem()
-	if err != nil {
-		log.Printf("Monitor memory error: %v", err)
-	}
-	log.Println("Available Memory:", dataMem.Available, "\nUsed Memory:", data.Used, "\nTotal Memory:", dataMem.TotalMemory, "\nUsedPercent Memory:", dataMem.UsedPercent, "\nFree Memory:", dataMem.Free)
-
-	dataLa, err := monitor.LoadAverage()
-	if err != nil {
-		log.Printf("Load Average error: %v", err)
-	}
-	log.Println("Load Average", dataLa.Load1, dataLa.Load5, dataLa.Load15)
-
-	dataCpuTemp, err := monitor.SystemUpTime()
-	if err != nil {
-		log.Printf("System Uptime error %v", err)
-	}
-	log.Println("System time:", dataCpuTemp.Days, "days", dataCpuTemp.Hours, "hours", dataCpuTemp.Minutes, "minutes")
+	monitor.recordmetrics()
 }
--- internal/monitor/metrics.go
+++ internal/monitor/metrics.go
@@ -1,42 +1,102 @@
 package monitor
 
 import (
-	"github.com/prometheus/client_golang/prometheus" // blad dla wersji prometheus, pobawic sie z poprawna wersja Go
+	"github.com/prometheus/client_golang/prometheus"
 )
 
 var (
 	CpuGauge = prometheus.NewGauge(prometheus.GaugeOpts{ // CpuGauge ma dostawac informacje z funkcja, dlatego funkcje nalezy zmienic lekko tak jak GPT zaproponwoal ale moim rozwiazaniem jest struct czyli niech pobiera z struct
-		Name: "monitor_cpu_percent",        // nazwa metryki
-		Help: "Monitor of CPU in percents", // opis metryki a dokladnie co metryka mierzy
+		Name: "monitor_cpu_percent",
+		Help: "Monitor of CPU in percents",
 	})
-
 	MonitorUsedGauge = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "monitor_used_of_memory",               // nazwa metryki
-		Help: "Monitor of Used Memory in the system", // opis metryki a dokladnie co metryka mierzy
+		Name: "monitor_used_of_memory",
+		Help: "Monitor of Used Memory in the system",
 	})
-
 	MonitorUsePercent = prometheus.NewGauge(prometheus.GaugeOpts{
 		Name: "monitor_use_percent_of_memory",
 		Help: "Monitor of Usepercent Memory in the system",
 	})
-
 	MonitorFreeMemory = prometheus.NewGauge(prometheus.GaugeOpts{
 		Name: "monitor_free_memory",
 		Help: "Monitor of Free Memory in the system",
 	})
-
 	MonitorAvaMemory = prometheus.NewGauge(prometheus.GaugeOpts{
 		Name: "monitor_available_memory",
 		Help: "Monitor of Available Memory in the system",
 	})
-
 	MonitorTotalMemory = prometheus.NewGauge(prometheus.GaugeOpts{
 		Name: "monitor_total_memory",
 		Help: "Monitor of Total Memory in the system",
 	})
-	// tu skonczyelem, poprawic ponizsza metryke i dodac kolejne dotyczace tej funkcji + dla systemupTiem i tu chyba uzyc counter
-	LoadAvarageGauge = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "monitor_cpu_percent",        // nazwa metryki
-		Help: "Monitor of CPU in percents", // opis metryki a dokladnie co metryka mierzy
+	LoadAvarage1Gauge = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "monitor_load_avarage_1",
+		Help: "Monitor of Load Avarage 1",
+	})
+	LoadAvarage5Gauge = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "monitor_load_avarage_5",
+		Help: "Monitor of Load Avarage 5",
+	})
+	LoadAvarage15Gauge = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "monitor_load_avarage_15",
+		Help: "Monitor of Load Avarage 15",
+	})
+	SystemUpTimeDaysGauge = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "monitor_system_up_time_in_days",
+		Help: "Monitor of SystemUp time in days",
+	})
+	SystemUpTimeHoursGauge = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "monitor_system_up_time_in_hours",
+		Help: "Monitor of SystemUp time in hours",
+	})
+	SystemUpTimeMinutesGauge = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "monitor_system_up_time_in_minutes",
+		Help: "Monitor of SystemUp time in minutes",
 	})
 )
+
+// rejestracja metryk w prometheusie, rejesttruje pojemniki "gauge "z metrykiami dla poszczegolnych funkcji
+func init() {
+	prometheus.MustRegister(
+		CpuGauge,
+		MonitorUsedGauge,
+		MonitorUsePercent,
+		MonitorFreeMemory,
+		MonitorAvaMemory,
+		MonitorTotalMemory,
+		LoadAvarage1Gauge,
+		LoadAvarage5Gauge,
+		LoadAvarage15Gauge,
+		SystemUpTimeDaysGauge,
+		SystemUpTimeHoursGauge,
+		SystemUpTimeMinutesGauge,
+	)
+}
+
+// funckcja cyklicznie analizuje dane z metryk
+func recordmetrics() {
+	cpuStat, err := MonitorCpu()
+	if err == nil {
+		CpuGauge.Set(cpuStat.CPU)
+	}
+	memStat, err := MonitorMem()
+	if err == nil {
+		MonitorUsedGauge.Set(memStat.Used)
+		MonitorUsePercent.Set(memStat.UsedPercent)
+		MonitorFreeMemory.Set(memStat.Free)
+		MonitorAvaMemory.Set(memStat.Available)
+		MonitorTotalMemory.Set(memStat.TotalMemory)
+	}
+	loadStat, err := LoadAverage()
+	if err == nil {
+		LoadAvarage1Gauge.Set(loadStat.Load1)
+		LoadAvarage5Gauge.Set(loadStat.Load5)
+		LoadAvarage15Gauge.Set(loadStat.Load15)
+	}
+	sysuptimeStat, err := SystemUpTime()
+	if err == nil {
+		SystemUpTimeDaysGauge.Set(float64(sysuptimeStat.Days))
+		SystemUpTimeHoursGauge.Set(float64(sysuptimeStat.Hours))
+		SystemUpTimeMinutesGauge.Set(float64(sysuptimeStat.Minutes))
+	}
+}
